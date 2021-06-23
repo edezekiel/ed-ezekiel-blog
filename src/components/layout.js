badf82d2e070ea3128ce7fee298336e0f4dc7002
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-06-23 10:08:10 -0500

    Refactor Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,47 +1,19 @@
-import * as React from "react";
-import { Link, useStaticQuery, graphql } from "gatsby";
-import { 
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle
- } from "./layout.module.css";
+import * as React from "react"
+import { container } from "./layout.module.css"
+import Footer from "./footer"
+import Header from "./header"
+import Nav from "./nav"
 
 const Layout = ({ pageTitle, children }) => {
-  const data = useStaticQuery(graphql`
-    query HeaderQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <main className={container}>
-      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <p className={siteTitle}>{data.site.siteMetadata.title}</p>
-      <header>
-        <nav>
-          <ul className={navLinks}>
-            <li className={navLinkItem}>
-              <Link to ="/" className={navLinkText}>Home</Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to ="/about" className={navLinkText}>About</Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to ="/blog" className={navLinkText}>Blog</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <h1 className={heading}>{pageTitle}</h1>
-      {children}
-    </main>
+    <>
+      <Nav></Nav>
+      <main className={container}>
+        <Header pageTitle={pageTitle}></Header>
+        {children}
+      </main>
+      <Footer></Footer>
+    </>
   )
 }
 
